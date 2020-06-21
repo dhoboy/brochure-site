131f75d9f58f663a42613ab4110bd3aa509056f6
Daniel Overstreet <overstreet@collectivedigitalstudio.com>
2020-06-22 01:16:38 +0900

    Updates

---
+++ precache-manifest.37ca293b6e65c57be435c6019c312733.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5c5440ac1e0c55fbdfc5e39b9f5081",
+    "url": "/brochure-site/index.html"
+  },
+  {
+    "revision": "2d5f0bc7ac6d6e73f578",
+    "url": "/brochure-site/static/css/main.a1ecaf14.chunk.css"
+  },
+  {
+    "revision": "260caa93c7f387ff4585",
+    "url": "/brochure-site/static/js/2.fa8a6499.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brochure-site/static/js/2.fa8a6499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5f0bc7ac6d6e73f578",
+    "url": "/brochure-site/static/js/main.11aded7a.chunk.js"
+  },
+  {
+    "revision": "1472a13092b7a70ef31b",
+    "url": "/brochure-site/static/js/runtime-main.5499c815.js"
+  },
+  {
+    "revision": "b68d3265c3091c7e6d3a8d39c31c13e1",
+    "url": "/brochure-site/static/media/Drew_Grasty.b68d3265.jpg"
+  },
+  {
+    "revision": "31b90b23d14272d828c9a9a2a3479351",
+    "url": "/brochure-site/static/media/Pouring.31b90b23.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/brochure-site/precache-manifest.37ca293b6e65c57be435c6019c312733.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brochure-site/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
